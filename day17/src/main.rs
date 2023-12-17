d1db75c81d5122f8da86fd22913fa756df41dc34
Kamal Ahmad <kamalahmad22@pm.me>
2023-12-17 14:25:46 +0500

    SPEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEED

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -3,15 +3,17 @@ use std::{
     collections::{BinaryHeap, HashMap, HashSet},
     hash::Hash,
 };
+use rustc_hash::FxHashMap;
+use rustc_hash::FxHashSet;
 
 const INPUT: &'static str = include_str!("../input.txt");
 const TEST: &'static str = include_str!("../test.txt");
 
 #[derive(PartialOrd, Ord, Clone, Copy, Eq, Debug)]
 struct Node {
-    distance: Reverse<u64>,
-    coords: (i64, i64),
-    dir: (i64, i64),
+    distance: Reverse<u32>,
+    coords: (i32, i32),
+    dir: (i32, i32),
     same_dir_count: usize,
 }
 
@@ -30,7 +32,7 @@ impl PartialEq for Node {
     }
 }
 
-fn adj(dir: (i64, i64)) -> [(i64, i64); 3] {
+fn adj(dir: (i32, i32)) -> [(i32, i32); 3] {
     match dir {
         (1, 0) => [(0, 1), (0, -1), (1, 0)],
         (-1, 0) => [(0, 1), (0, -1), (-1, 0)],
@@ -40,68 +42,77 @@ fn adj(dir: (i64, i64)) -> [(i64, i64); 3] {
     }
 }
 
-struct Grid(Vec<u64>, usize);
+struct Grid(Vec<u32>, usize);
 
 impl Grid {
-    fn get(&self, idx: (i64, i64)) -> Option<&u64> {
+    fn get(&self, idx: (i32, i32)) -> Option<&u32> {
         if idx.0 < 0 || idx.1 < 0 || idx.0 as usize >= self.0.len() / self.1 || idx.1 as usize >= self.1 {
             None
         }
         else {
-            self.0.get((idx.0 * self.1 as i64 + idx.1) as usize)
+            self.0.get((idx.0 * self.1 as i32 + idx.1) as usize)
         }
     }
 }
 
-impl std::ops::Index<(i64, i64)> for Grid {
-    type Output = u64;
-    fn index(&self, index: (i64, i64)) -> &Self::Output {
-        self.get(index).unwrap()
+impl std::ops::Index<(i32, i32)> for Grid {
+    type Output = u32;
+    fn index(&self, index: (i32, i32)) -> &Self::Output {
+        unsafe { self.get(index).unwrap_unchecked() }
+        //self.get(index).unwrap()
     }
 }
 
 fn parse_grid(input: &str) -> Grid {
-    let cols = input.lines().next().unwrap().len();
+    let cols = unsafe { input.lines().next().unwrap_unchecked().len() } ;
     let grid = input
         .lines()
         .flat_map(|l| {
             l.chars()
-                .map(move |c| c.to_digit(10).unwrap() as u64)
+                .map(move |c| c.to_digit(10).unwrap() as u32)
         })
-        .collect::<Vec<u64>>();
+        .collect::<Vec<u32>>();
     Grid(grid, cols)
 }
 
-fn run(grid2: &Grid, min_steps: usize, max_steps: usize) {
+fn mhd(a: (i32, i32), b: (i32, i32)) -> u32 {
+    ((a.0 - b.0).abs() + (a.1 - b.1).abs()) as u32
+    //0
+}
+
+fn run(grid: &Grid, min_steps: usize, max_steps: usize) {
     let dest = (
-        (grid2.0.len() / grid2.1) as i64 - 1,
-        grid2.1 as i64 - 1
+        (grid.0.len() / grid.1) as i32 - 1,
+        grid.1 as i32 - 1
     );
 
     let mut heap = BinaryHeap::new();
-    let mut dists = HashMap::new();
-    let mut visited = HashSet::new();
+    let mut dists = FxHashMap::default();
+    let mut visited = FxHashSet::default();
     let node1 = Node {
-        distance: Reverse(grid2[(0, 1)]),
+        distance: Reverse(grid[(0, 1)] + mhd((0, 1), dest)),
         coords: (0, 1),
         dir: (0, 1),
         same_dir_count: 1,
     };
     heap.push(node1);
-    dists.insert(node1, node1.distance.0);
+    dists.insert(node1, grid[(0, 1)]);
     let node2 = Node {
-        distance: Reverse(grid2[(1, 0)]),
+        distance: Reverse(grid[(1, 0)] + mhd((1, 0), dest)),
         coords: (1, 0),
         dir: (1, 0),
         same_dir_count: 1,
     };
     heap.push(node2);
-    dists.insert(node2, node2.distance.0);
+    dists.insert(node2, grid[(1, 0)]);
 
+    let mut visited_count = 0;
     while heap.len() != 0 {
+        visited_count+=1;
         let cur = heap.pop().unwrap();
         if cur.coords == dest {
             println!("Found dest! Heat loss: {:?}", cur.distance);
+            println!("Visited {} nodes", visited_count);
             break;
         }
         visited.insert(cur);
@@ -113,7 +124,7 @@ fn run(grid2: &Grid, min_steps: usize, max_steps: usize) {
                 continue;
             }
             let pos = (cur.coords.0 + adj.0, cur.coords.1 + adj.1);
-            if grid2.get(pos).is_none() {
+            if grid.get(pos).is_none() {
                 continue;
             }
             let same_dir_count = if adj == cur.dir {
@@ -121,9 +132,8 @@ fn run(grid2: &Grid, min_steps: usize, max_steps: usize) {
             } else {
                 1
             };
-            let cur_pos_dist = dists[&cur] + grid2[pos];
-            let adj_node = Node {
-                distance: Reverse(cur_pos_dist),
+            let mut adj_node = Node {
+                distance: Reverse(0), // needless optimization
                 coords: pos,
                 dir: adj,
                 same_dir_count,
@@ -131,29 +141,24 @@ fn run(grid2: &Grid, min_steps: usize, max_steps: usize) {
             if visited.contains(&adj_node) {
                 continue;
             }
-            let dist = dists.entry(adj_node).or_insert(u64::MAX);
+            let cur_pos_dist = dists[&cur] + grid[pos];
+            adj_node.distance.0 = cur_pos_dist + mhd(pos, dest);
+            let dist = dists.entry(adj_node).or_insert(u32::MAX);
             if cur_pos_dist < *dist {
                 *dist = cur_pos_dist;
                 heap.push(adj_node);
             }
         }
-        while heap
-            .peek()
-            .map(|top| visited.contains(&top))
-            .unwrap_or(false)
-        {
-            dbg!(heap.pop());
-        }
-    }
+   }
     //println!("{:?}", dists);
 }
 
-fn print_grid(grid: &HashMap<(i64, i64), u64>, prev: &HashMap<(i64, i64), (i64, i64)>) {
+fn print_grid(grid: &FxHashMap<(i32, i32), u32>, prev: &FxHashMap<(i32, i32), (i32, i32)>) {
     let min_y = grid.keys().map(|(y, _)| *y).min().unwrap();
     let max_y = grid.keys().map(|(y, _)| *y).max().unwrap();
-    let min_x: i64 = grid.keys().map(|(_, x)| *x).min().unwrap();
+    let min_x: i32 = grid.keys().map(|(_, x)| *x).min().unwrap();
     let max_x = grid.keys().map(|(_, x)| *x).max().unwrap();
-    let mut stack = HashMap::new();
+    let mut stack = FxHashMap::default();
     let mut cur = (max_y, max_x);
     while prev.get(&cur).is_some() {
         let prev_node = prev.get(&cur).unwrap();
